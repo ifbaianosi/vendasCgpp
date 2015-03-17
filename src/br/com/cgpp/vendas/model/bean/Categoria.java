3bd6df84f7b636d6e1dc7cb3a2346c2a82677e42
glaubermatos <glaub.oliveira@hotmail.com>
2015-03-17 08:31:00 -0300

    2 classes bean prontas
    Categoria e Produto

---
+++ src/br/com/cgpp/vendas/model/bean/Categoria.java
@@ -0,0 +1,36 @@
+package br.com.cgpp.vendas.model.bean;
+
+
+public class Categoria {
+
+	private int idcategoria;
+	private String nome;
+	private String descricao;
+
+	public int getIdcategoria() {
+		return idcategoria;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setIdcategoria(int idcategoria) {
+		this.idcategoria = idcategoria;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+}
